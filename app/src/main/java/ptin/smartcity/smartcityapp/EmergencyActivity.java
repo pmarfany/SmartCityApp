a53498926c3a4d4ea57cf338a43bcf3efe1eaa9c
Pau Marfany <p.marfany_96@hotmail.com>
2017-05-11 16:17:14 +0200

    Added menú

--- app/src/main/java/ptin/smartcity/smartcityapp/EmergencyActivity.java
+++ app/src/main/java/ptin/smartcity/smartcityapp/EmergencyActivity.java
@@ -7,6 +7,7 @@ import android.graphics.drawable.AnimationDrawable;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -15,12 +16,16 @@ public class EmergencyActivity extends AppCompatActivity {
 
     @Bind(R.id.loading_dots) ImageView img;
     @Bind(R.id.ambulance) ImageView ambulance;
+    @Bind(R.id.emergency_text) TextView emergency_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergency);
         ButterKnife.bind(this);
+
+        // Set text
+        setEmergencyText();
 
         // Shaking car
         Animation shake = AnimationUtils.loadAnimation(this, R.anim.anim_shake);
@@ -34,5 +39,23 @@ public class EmergencyActivity extends AppCompatActivity {
 
         // Start the animation (looped playback by default).
         frameAnimation.start();
+    }
+
+    public void setEmergencyText() {
+        // Posem el text en funció del servei
+        String option = getIntent().getStringExtra("EMERGENCY_OPTION");
+
+        switch (option) {
+            case "AMBULANCE":
+                emergency_text.setText(R.string.ambulance_message);
+                break;
+            case "FIREFIGHTER":
+                emergency_text.setText(R.string.firefighter_message);
+                break;
+            case "POLICE":
+                emergency_text.setText(R.string.police_message);
+                break;
+        }
+
     }
 }
--- app/src/main/java/ptin/smartcity/smartcityapp/MainActivity.java
+++ app/src/main/java/ptin/smartcity/smartcityapp/MainActivity.java
@@ -2,110 +2,158 @@ package ptin.smartcity.smartcityapp;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Patterns;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import butterknife.ButterKnife;
 import butterknife.Bind;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener,
+                   View.OnClickListener {
 
-    // Obtenim els camps de la pantalla
-    @Bind(R.id.IP_input) EditText _IPField;
-    @Bind(R.id.btn_sos) ImageButton _SOSButton;
+    // Layout del menú lateral
+    private DrawerLayout drawer;
+
+    // Botó de SOS
+    private ImageButton _SOSButton;
+
+    // Sel·lecció del servei d'emergencies
+    private String EMERGENCY_OPTION = "AMBULANCE";
+
+    // "Accés" pantalla emergències
+    Intent emergency;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
 
-        // Inicialitzem la pantalla de login
+        /* Inicialitzem la pantalla de login
         Intent intent = new Intent(this, LoginActivity.class);
-        //startActivity(intent);
+        startActivity(intent);*/
+
+        // Configurar toolbar de l'aplicació
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        // Configurar layout principal
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Configurar menú lateral
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        // Seleccionem opció "Ambulance" per defecte
+        navigationView.setCheckedItem(R.id.nav_ambulance);
+
+        // SOSButton: Comportament
+        _SOSButton = (ImageButton) findViewById(R.id.btn_sos);
+        _SOSButton.setOnClickListener(this);
 
         // Inicialitzem la pantalla d'emergències
-        final Intent emergency = new Intent(this, EmergencyActivity.class);
-
-        // SignUp Button: Comportament
-        _SOSButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-            _SOSButton.setEnabled(false);
-
-            if ( !checkIP() ) {
-                Toast.makeText(getBaseContext(), "IP Address not valid", Toast.LENGTH_SHORT).show();
-                _SOSButton.setEnabled(true);
-                return;
-            }
-
-            // TODO: Comportament (localització)
-            EmergencySocket communicacio = new EmergencySocket( _IPField.getText().toString() );
-            communicacio.sendMessage("SOS - Android App");
-            communicacio.close();
-
-            final ProgressDialog progressDialog = new ProgressDialog(MainActivity.this, R.style.LoginTheme_Dialog);
-            progressDialog.setIndeterminate(true);
-            progressDialog.setMessage( "Sending message" );
-            progressDialog.show();
-
-            // TEMPORAL: després de 3 segons, Missatge Enviat
-            new android.os.Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        progressDialog.dismiss();
-
-                        // Iniciem la pantalla d'emergències
-                        startActivity(emergency);
-                    }
-                }, 3000);
-
-            _SOSButton.setEnabled(true);
-            }
-        });
-
+        emergency = new Intent(this, EmergencyActivity.class);
     }
 
-    // Check IP Address
+    /* Check IP Address
     public boolean checkIP() {
         // Obtenir valors
         String ip_address = _IPField.getText().toString();
 
         // EMAIL: Comprovar que el camp no estigui buit i que sigui una adreça IP
-        if ( ip_address.isEmpty() || !Patterns.IP_ADDRESS.matcher(ip_address).matches() ) {
-            _IPField.setError( "IP Address not valid" );
+        if (ip_address.isEmpty() || !Patterns.IP_ADDRESS.matcher(ip_address).matches()) {
+            _IPField.setError("IP Address not valid");
             return false;
         } else {
             _IPField.setError(null);
             return true;
         }
+    }*/
+
+
+    @Override
+    public void onBackPressed() {
+        // Si el menú lateral està obert, tancar-lo.
+        if (drawer.isDrawerOpen(GravityCompat.START)) drawer.closeDrawer(GravityCompat.START);
+
+            // En cas contrari, tancar l'aplicació.
+        else super.onBackPressed();
     }
 
-    /*@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.nav_ambulance:
+                EMERGENCY_OPTION = "AMBULANCE";
+                break;
+            case R.id.nav_firefighter:
+                EMERGENCY_OPTION = "FIREFIGHTER";
+                break;
+            case R.id.nav_police:
+                EMERGENCY_OPTION = "POLICE";
+                break;
+            case R.id.nav_settings:
+                // Handle the settings action
+                break;
+        }
+
+        // Tanquem el menú
+        drawer.closeDrawer(GravityCompat.START);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onClick(View v) {
+        // Desactivem el botó
+        _SOSButton.setEnabled(false);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        // TODO: Comportament (localització)
 
-        return super.onOptionsItemSelected(item);
-    }*/
+        // Iniciem la comunicació
+        //EmergencySocket communicacio = new EmergencySocket( _IPField.getText().toString() );
+        //communicacio.sendMessage("SOS - Android App");
+        //communicacio.close();
+
+        final ProgressDialog progressDialog = new ProgressDialog(MainActivity.this, R.style.LoginTheme_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Sending message");
+        progressDialog.show();
+
+        // TEMPORAL: després de 3 segons, Missatge Enviat
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        progressDialog.dismiss();
+
+                        _SOSButton.setEnabled(true);
+
+                        // Iniciem la pantalla d'emergències
+                        emergency.putExtra("EMERGENCY_OPTION", EMERGENCY_OPTION);
+                        startActivity(emergency);
+                    }
+                }, 3000);
+    }
 }
